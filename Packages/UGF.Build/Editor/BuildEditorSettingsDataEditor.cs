542bc17a40c94f109bf126e21be18830c67ef0f6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-18 21:46:51 +0300

    Add BuildEditorSettingsDataPlatformsDrawer

--- Packages/UGF.Build/Editor/BuildEditorSettingsDataEditor.cs
+++ Packages/UGF.Build/Editor/BuildEditorSettingsDataEditor.cs
@@ -1,9 +1,40 @@
-﻿using UnityEditor;
+﻿using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
 
 namespace UGF.Build.Editor
 {
     [CustomEditor(typeof(BuildEditorSettingsData), true)]
     internal class BuildEditorSettingsDataEditor : UnityEditor.Editor
     {
+        private readonly BuildEditorSettingsDataPlatformsDrawer m_platformsDrawer = new BuildEditorSettingsDataPlatformsDrawer();
+        private SerializedProperty m_propertyLogEnable;
+        private SerializedProperty m_propertyLogFilter;
+        private SerializedProperty m_propertyPlatformsGroups;
+
+        private void OnEnable()
+        {
+            m_platformsDrawer.Enable();
+
+            m_propertyLogEnable = serializedObject.FindProperty("m_logEnable");
+            m_propertyLogFilter = serializedObject.FindProperty("m_logFilter");
+            m_propertyPlatformsGroups = serializedObject.FindProperty("m_platforms.m_groups");
+        }
+
+        private void OnDisable()
+        {
+            m_platformsDrawer.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorGUILayout.PropertyField(m_propertyLogEnable);
+                EditorGUILayout.PropertyField(m_propertyLogFilter);
+                EditorGUILayout.Space();
+
+                m_platformsDrawer.DrawGUILayout(m_propertyPlatformsGroups);
+            }
+        }
     }
 }
---
+++ Packages/UGF.Build/Editor/BuildEditorSettingsDataPlatformsDrawer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.PlatformSettings;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.EditorTools.Editor.Platforms;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Build.Editor
+{
+    internal class BuildEditorSettingsDataPlatformsDrawer : PlatformSettingsDrawer
+    {
+        private readonly Dictionary<string, ReorderableListDrawer> m_listDrawers = new Dictionary<string, ReorderableListDrawer>();
+        private static float m_padding = 5F;
+
+        public BuildEditorSettingsDataPlatformsDrawer()
+        {
+            AllowEmptySettings = false;
+            AutoSettingsInstanceCreation = true;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            AddPlatformAllAvailable();
+
+            for (int i = 0; i < PlatformEditorUtility.PlatformsAllAvailable.Count; i++)
+            {
+                PlatformInfo platform = PlatformEditorUtility.PlatformsAllAvailable[i];
+                string name = platform.BuildTargetGroup.ToString();
+
+                AddGroupType(name, typeof(BuildPlatformSettings));
+            }
+
+            foreach (KeyValuePair<string, ReorderableListDrawer> pair in m_listDrawers)
+            {
+                pair.Value.Enable();
+            }
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            ClearGroups();
+            ClearGroupTypes();
+
+            foreach (KeyValuePair<string, ReorderableListDrawer> pair in m_listDrawers)
+            {
+                pair.Value.Disable();
+            }
+
+            m_listDrawers.Clear();
+        }
+
+        protected override void OnDrawSettings(Rect position, SerializedProperty propertyGroups, string name)
+        {
+            float height = EditorGUIUtility.singleLineHeight;
+            float space = EditorGUIUtility.standardVerticalSpacing;
+
+            PlatformInfo platform = GetPlatformInfo(name);
+            SerializedProperty propertySettings = OnGetSettings(propertyGroups, name);
+
+            using (new LabelWidthChangeScope(-m_padding))
+            {
+                var rectPlatformName = new Rect(position.x, position.y, position.width, height);
+                var rectProfiles = new Rect(position.x, rectPlatformName.yMax + space, position.width, height);
+
+                EditorGUI.LabelField(rectPlatformName, $"Settings for {platform.Label.text}");
+
+                GetListDrawer(name, propertySettings).DrawGUI(rectProfiles);
+            }
+        }
+
+        protected override float OnGetSettingsHeight(SerializedProperty propertyGroups)
+        {
+            float height = EditorGUIUtility.singleLineHeight;
+            float space = EditorGUIUtility.standardVerticalSpacing;
+
+            string name = GetSelectedGroupName();
+            SerializedProperty propertySettings = OnGetSettings(propertyGroups, name);
+            SerializedProperty propertyProfiles = propertySettings.FindPropertyRelative("m_profiles");
+
+            float heightProfiles = propertyProfiles.isExpanded ? EditorGUI.GetPropertyHeight(propertyProfiles) + height + space : height;
+
+            return height + space * 2F + heightProfiles + m_padding * 2F;
+        }
+
+        protected override void OnCreateSettings(SerializedProperty propertyGroups, string name, SerializedProperty propertySettings)
+        {
+            base.OnCreateSettings(propertyGroups, name, propertySettings);
+
+            GetListDrawer(name, propertySettings);
+        }
+
+        private PlatformInfo GetPlatformInfo(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            var buildTargetGroup = (BuildTargetGroup)Enum.Parse(typeof(BuildTargetGroup), name, true);
+            PlatformInfo platform = PlatformEditorUtility.GetPlatform(buildTargetGroup);
+
+            return platform;
+        }
+
+        private ReorderableListDrawer GetListDrawer(string name, SerializedProperty propertySettings)
+        {
+            if (!m_listDrawers.TryGetValue(name, out ReorderableListDrawer drawer))
+            {
+                SerializedProperty propertyProfiles = propertySettings.FindPropertyRelative("m_profiles");
+
+                drawer = new ReorderableListDrawer(propertyProfiles);
+
+                m_listDrawers.Add(name, drawer);
+            }
+
+            return drawer;
+        }
+    }
+}
