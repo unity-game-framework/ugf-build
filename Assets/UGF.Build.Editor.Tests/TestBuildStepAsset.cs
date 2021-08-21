3a31fc0faf07ff01b6449128fe9a3d502eca6ac8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-21 19:32:39 +0300

    Update tests

---
+++ Assets/UGF.Build.Editor.Tests/TestBuildStepAsset.cs
@@ -0,0 +1,25 @@
+﻿using UGF.RuntimeTools.Runtime.Contexts;
+using UnityEngine;
+
+namespace UGF.Build.Editor.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestBuildStepAsset")]
+    public class TestBuildStepAsset : BuildStepAsset
+    {
+        protected override IBuildStep OnBuild()
+        {
+            return new TestBuildStep(name);
+        }
+    }
+
+    public class TestBuildStep : BuildStep
+    {
+        public TestBuildStep(string name) : base(name)
+        {
+        }
+
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+        }
+    }
+}
