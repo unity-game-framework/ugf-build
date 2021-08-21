7644836cb5be02441471616c740842a6f3d2edd0
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-21 19:20:34 +0300

    Add BuildSetupAssetEditor

---
+++ Packages/UGF.Build/Editor/BuildSetupAssetEditor.cs
@@ -0,0 +1,34 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.EnabledProperty;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+
+namespace UGF.Build.Editor
+{
+    [CustomEditor(typeof(BuildSetupAsset), true)]
+    internal class BuildSetupAssetEditor : UnityEditor.Editor
+    {
+        private EnabledPropertyListDrawer m_listSteps;
+
+        private void OnEnable()
+        {
+            m_listSteps = new EnabledPropertyListDrawer(serializedObject.FindProperty("m_steps"));
+            m_listSteps.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listSteps.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listSteps.DrawGUILayout();
+            }
+        }
+    }
+}
