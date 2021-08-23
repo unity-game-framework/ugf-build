f4c27b56ea5485e639889ddf8ab60b0ac56a429e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-23 18:56:53 +0300

    Update BuildEditorSettingsDataEditor.cs

--- Packages/UGF.Build/Editor/BuildEditorSettingsDataEditor.cs
+++ Packages/UGF.Build/Editor/BuildEditorSettingsDataEditor.cs
@@ -9,6 +9,7 @@ namespace UGF.Build.Editor
         private readonly BuildEditorSettingsDataPlatformsDrawer m_platformsDrawer = new BuildEditorSettingsDataPlatformsDrawer();
         private SerializedProperty m_propertyLogEnable;
         private SerializedProperty m_propertyLogFilter;
+        private SerializedProperty m_propertySetupNameEnvironmentVariableName;
         private SerializedProperty m_propertyPlatformsGroups;
 
         private void OnEnable()
@@ -17,6 +18,7 @@ namespace UGF.Build.Editor
 
             m_propertyLogEnable = serializedObject.FindProperty("m_logEnable");
             m_propertyLogFilter = serializedObject.FindProperty("m_logFilter");
+            m_propertySetupNameEnvironmentVariableName = serializedObject.FindProperty("m_setupNameEnvironmentVariableName");
             m_propertyPlatformsGroups = serializedObject.FindProperty("m_platforms.m_groups");
         }
 
@@ -31,6 +33,7 @@ namespace UGF.Build.Editor
             {
                 EditorGUILayout.PropertyField(m_propertyLogEnable);
                 EditorGUILayout.PropertyField(m_propertyLogFilter);
+                EditorGUILayout.PropertyField(m_propertySetupNameEnvironmentVariableName);
                 EditorGUILayout.Space();
 
                 m_platformsDrawer.DrawGUILayout(m_propertyPlatformsGroups);
