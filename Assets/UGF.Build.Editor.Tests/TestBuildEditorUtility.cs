af34d4ed375f4e0a95e893e0718d8ce7166634bc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-04 16:13:56 +0300

    Add tests

---
+++ Assets/UGF.Build.Editor.Tests/TestBuildEditorUtility.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UGF.EditorTools.Editor.Yaml;
+using UnityEditor.Build.Reporting;
+
+namespace UGF.Build.Editor.Tests
+{
+    public class TestBuildEditorUtility
+    {
+        [Test]
+        public void TryGetBuildReport()
+        {
+            var result = EditorYamlUtility.FromYamlAtPath<BuildReport>("Assets/StreamingAssets/LastBuild.buildreport");
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<BuildReport>(result);
+        }
+    }
+}
