59461f346c52065e4bc52195f1f9ad70520c2973
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 20:26:16 +0100

    Add UnityCloudBuildManifestReleaseNoteStep

---
+++ Packages/UGF.Build/Editor/UnityCloud/UnityCloudBuildManifestReleaseNoteStep.cs
@@ -0,0 +1,17 @@
+﻿using UGF.Build.Editor.ReleaseNotes;
+using UGF.Build.Runtime.UnityCloud;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Build.Editor.UnityCloud
+{
+    public class UnityCloudBuildManifestReleaseNoteStep : BuildStep
+    {
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+            var releaseNote = context.Get<ReleaseNoteData>();
+            var manifest = context.Get<UnityCloudBuildManifest>();
+
+            releaseNote.AddData(manifest);
+        }
+    }
+}
