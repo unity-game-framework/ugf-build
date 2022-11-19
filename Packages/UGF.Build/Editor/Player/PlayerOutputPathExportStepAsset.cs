a53952e4357626425e1950246ed14a846a3cfeb4
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 20:53:27 +0100

    Add PlayerOutputPathExportStep

---
+++ Packages/UGF.Build/Editor/Player/PlayerOutputPathExportStepAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UGF.Build.Editor.Player
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Build/Player Output Path Export Step", order = 2000)]
+    public class PlayerOutputPathExportStepAsset : BuildStepAsset
+    {
+        [SerializeField] private string m_path;
+
+        public string Path { get { return m_path; } set { m_path = value; } }
+
+        protected override IBuildStep OnBuild()
+        {
+            return new PlayerOutputPathExportStep(m_path);
+        }
+    }
+}
