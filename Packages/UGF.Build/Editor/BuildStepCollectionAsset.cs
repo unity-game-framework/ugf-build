bab3a87cd25ba13d231078d8bc0a8f23d891ce09
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 18:44:08 +0100

    Add BuildStepCollectionListAsset

---
+++ Packages/UGF.Build/Editor/BuildStepCollectionAsset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.Build.Editor
+{
+    public abstract class BuildStepCollectionAsset : ScriptableObject
+    {
+        public void GetSteps(ICollection<IBuildStep> steps)
+        {
+            if (steps == null) throw new ArgumentNullException(nameof(steps));
+
+            OnGetSteps(steps);
+        }
+
+        protected abstract void OnGetSteps(ICollection<IBuildStep> steps);
+    }
+}
---
+++ Packages/UGF.Build/Editor/BuildStepCollectionListAsset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.Build.Editor
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Build/Build Step Collection List", order = 2000)]
+    public class BuildStepCollectionListAsset : BuildStepCollectionAsset
+    {
+        [SerializeField] private List<BuildStepAsset> m_steps = new List<BuildStepAsset>();
+
+        public List<BuildStepAsset> Steps { get { return m_steps; } }
+
+        protected override void OnGetSteps(ICollection<IBuildStep> steps)
+        {
+            for (int i = 0; i < m_steps.Count; i++)
+            {
+                BuildStepAsset step = m_steps[i];
+
+                steps.Add(step.Build());
+            }
+        }
+    }
+}
