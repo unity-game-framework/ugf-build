9803492c3f8982440f225d69d77df81b876778fc
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 21:20:46 +0100

    Add UnityCloudAndroidVersionCodeStep

---
+++ Packages/UGF.Build/Editor/UnityCloud.Android/UnityCloudAndroidVersionCodeStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UGF.Build.Runtime.UnityCloud;
+using UGF.RuntimeTools.Runtime.Contexts;
+using UnityEditor;
+
+namespace UGF.Build.Editor.UnityCloud.Android
+{
+    public class UnityCloudAndroidVersionCodeStep : BuildStep
+    {
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+            var manifest = context.Get<UnityCloudBuildManifest>();
+
+            if (!int.TryParse(manifest.BuildNumber, out int number))
+            {
+                throw new ArgumentException($"Failed to parse Unity Cloud Manifest build number: '{manifest.BuildNumber}'.");
+            }
+
+            PlayerSettings.Android.bundleVersionCode = number;
+        }
+    }
+}
