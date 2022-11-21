69dddeb05b3be5eab0765053f312e106dbb1737a
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-21 17:18:27 +0100

    Add PlayerScenesStep

---
+++ Packages/UGF.Build/Editor/Player/PlayerScenesStep.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.EditorTools.Runtime.Ids;
+using UGF.RuntimeTools.Runtime.Contexts;
+using UnityEditor;
+
+namespace UGF.Build.Editor.Player
+{
+    public class PlayerScenesStep : BuildStep
+    {
+        public IReadOnlyList<GlobalId> SceneIds { get; }
+
+        public PlayerScenesStep(IReadOnlyList<GlobalId> sceneIds)
+        {
+            SceneIds = sceneIds ?? throw new ArgumentNullException(nameof(sceneIds));
+        }
+
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+            var scenes = new EditorBuildSettingsScene[SceneIds.Count];
+
+            for (int i = 0; i < SceneIds.Count; i++)
+            {
+                GlobalId sceneId = SceneIds[i];
+                string path = AssetDatabase.GUIDToAssetPath(sceneId.ToString());
+
+                scenes[i] = new EditorBuildSettingsScene(path, true);
+            }
+
+            EditorBuildSettings.scenes = scenes;
+        }
+    }
+}
---
+++ Packages/UGF.Build/Editor/Player/PlayerScenesStepAssetEditor.cs
@@ -0,0 +1,37 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+
+namespace UGF.Build.Editor.Player
+{
+    [CustomEditor(typeof(PlayerScenesStepAsset), true)]
+    internal class PlayerScenesStepAssetEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_listScenes;
+
+        private void OnEnable()
+        {
+            m_listScenes = new ReorderableListDrawer(serializedObject.FindProperty("m_scenes"))
+            {
+                DisplayAsSingleLine = true
+            };
+
+            m_listScenes.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listScenes.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listScenes.DrawGUILayout();
+            }
+        }
+    }
+}
