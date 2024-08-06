e08dd6e9a0ffea3997c768357527bbb33b6335d3
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-08-06 22:33:37 +0300

    Add FileStep

---
+++ Packages/UGF.Build/Editor/Files/FileStepAssetEditor.cs
@@ -0,0 +1,37 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+
+namespace UGF.Build.Editor.Files
+{
+    [CustomEditor(typeof(FileStepAsset), true)]
+    internal class FileStepAssetEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_listCopy;
+
+        private void OnEnable()
+        {
+            m_listCopy = new ReorderableListDrawer(serializedObject.FindProperty("m_copy"))
+            {
+                DisplayElementFoldout = false
+            };
+
+            m_listCopy.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listCopy.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listCopy.DrawGUILayout();
+            }
+        }
+    }
+}
